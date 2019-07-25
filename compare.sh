188ef42df278a4816e7ecd893bceb03b319fcbe7
Charles Severance <csev@umich.edu>
2019-07-25 06:10:49 -0400

    Add missing file. Update comparison script

--- compare.sh
+++ compare.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
-diff -rq . /Users/csev/dev/sakai-scripts/trunk/basiclti/tsugi-util | grep '.java differ$' | awk '{ print "diff ", $2, $4 }'
+diff -rq src /Users/csev/dev/sakai-scripts/trunk/basiclti/tsugi-util/src | grep '.java differ$' | awk '{ print "diff ", $2, $4 }'
+
+diff -rq src /Users/csev/dev/sakai-scripts/trunk/basiclti/tsugi-util/src | grep 'Only in /Users/csev/dev/sakai-scripts/trunk/basiclti/tsugi-util/src' | grep -v .DS_Store | sed 's"Only in /Users/csev/dev/sakai-scripts/trunk/basiclti/tsugi-util/src""' | sed 's": "/"' | awk '{ print "cp /Users/csev/dev/sakai-scripts/trunk/basiclti/tsugi-util/src" $1, "src" $1 }'
+
